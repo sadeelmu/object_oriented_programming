4d40f3bc8f464b358d8ca7af753dbdc244ac1124
Sadel Muwahed <68566188+sadeelmu@users.noreply.github.com>
2023-02-27 07:02:43 +0300

    Create city.h

---
+++ city/city.h
@@ -0,0 +1,51 @@
+//Sadeel Muwahed 20200232 Lab 4 Section 8
+
+//header.h
+
+#ifndef CITY_H
+#define CITY_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class City{
+
+  private:
+    string name_;
+    float income_;
+    int numResidents_;
+
+    
+
+  public:
+    //default constructor
+    City();
+    //destructor
+    ~City();
+    //paramterized constructor
+    City(string name="", float income=0, int numResident=0);
+
+    //setters
+    void setName(string n);
+    void setIncome(float i);
+    void setNumResidents(int r);
+
+    //getters
+    string getName();
+    float getIncome();
+    int getNumResidents();
+
+    //function
+    void printDetails();
+    City combineCities(City c);
+    bool hasMoreIncome(City c);
+    float getCityIncome();
+    int addResidents(int n);
+    float updateIncome(int v);
+
+};
+
+
+#endif
+
+
