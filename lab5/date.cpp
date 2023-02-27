276392772884271c8917e631d6c5b3c60737ec20
Sadel Muwahed <68566188+sadeelmu@users.noreply.github.com>
2023-02-27 07:07:51 +0300

    Update date.cpp

--- lab5/date.cpp
+++ lab5/date.cpp
@@ -1,54 +1,36 @@
-#include "date.h"
+#ifndef D_H
+#define D_H
+
 #include <iostream>
-#include <string>
 using namespace std;
 
-Date::Date(unsigned int day = 1, unsigned int month = 1, unsigned int year = 1900)
-{
-    if (day > 12)
-    {
-        day = 12;
-    }
-    if (month == 2)
-    {
-        if (day > 28)
-        {
+class Date {
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
+
+public:
+    Date(unsigned int d=1, unsigned int m=1, unsigned int y=1900)
+        :day(d), month(m), year(y) {
+        if (month > 12)
+            month = 12;
+
+        if (month == 2 && day > 28)
             day = 28;
-        }
-    }
-    else if (month == 1 || month == 3 || month == 5 || month == 8 || month == 10 || month == 12)
-    {
-        if (day > 31)
-        {
+        else if ((month == 1 || month == 3  || month == 5 || month == 7 || 
+                 month == 8 || month == 10 || month == 12) && day > 31) {
             day = 31;
-        }
-    }
-    else
-    {
-        if (day > 30)
-        {
+        } else if (day > 30) {
             day = 30;
         }
     }
-}
-unsigned int Date::getDay()
-{
-    return day_;
-}
-unsigned int Date::getMonth()
-{
-    return month_;
-}
-unsigned int Date::getYear()
-{
-    return year_;
-}
 
-void Date::print()
-{
-    cout << day_;
-    cout << "/";
-    cout << month_;
-    cout << "/";
-    cout << year_;
-}
+    unsigned int get_day() const { return day; }
+    unsigned int get_month() const { return month; }
+    unsigned int get_year() const { return year; }
+
+    void print() const {
+        cout << day << "/" << month << "/" << year;
+    }
+};
+#endif
